5c251ed8ac05bbceaea213e38045fe1d553631da
Ryan Makoni <rmakoni@3-H-1.42heilbronn.de>
2025-03-31 16:45:00 +0200

    set up classes. Need to read up on how they work still

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,30 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/31 15:45:30 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/03/31 16:27:09 by rmakoni          ###   ########.fr       */
+/*   Created: 2025/03/31 16:22:45 by rmakoni           #+#    #+#             */
+/*   Updated: 2025/03/31 16:27:04 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cctype>
 #include <iostream>
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	else
-		for (int i = 1; i < argc; i++)
-		{
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)std::toupper(argv[i][j]);
-			if (i + 1 != argc)
-				std::cout << " ";
-		}
-	std::cout << std::endl;
-	return (0);
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -1,30 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/31 15:45:30 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/03/31 16:27:09 by rmakoni          ###   ########.fr       */
+/*   Created: 2025/03/31 16:22:42 by rmakoni           #+#    #+#             */
+/*   Updated: 2025/03/31 16:32:46 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cctype>
 #include <iostream>
 
-int	main(int argc, char **argv)
+class Contact
 {
-	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	else
-		for (int i = 1; i < argc; i++)
-		{
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)std::toupper(argv[i][j]);
-			if (i + 1 != argc)
-				std::cout << " ";
-		}
-	std::cout << std::endl;
-	return (0);
-}
+  private:
+	std::string firstName;
+	std::string lastName;
+	std::string phoneNumber;
+
+  public:
+	// Constructor
+	Contact();
+
+	// Setter methods
+	void setFirstName(const std::string &name);
+	void setLastName(const std::string &name);
+	void setPhoneNumber(const std::string &number);
+
+	// Getter methods
+	std::string getFirstName() const;
+	std::string getLastName() const;
+	std::string getPhoneNumber() const;
+};
