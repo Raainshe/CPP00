fd68ddf975182c2fccf772098f623281d065ee33
Ryan Makoni <rmakoni@1-B-32.42heilbronn.de>
2025-04-01 16:47:16 +0200

    understanding more of classes again.

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,12 +6,31 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 16:22:45 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/03/31 16:27:04 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/04/01 16:20:27 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "phonebook.hpp"
 #include <iostream>
+#include <string>
 
+int	valid_command(std::string command)
+{
+	if (command == "ADD")
+		return (1);
+	else if (command == "SEARCH")
+		return (2);
+	else if (command == "EXIT")
+		return (3);
+	return (-1);
+}
 int	main(int argc, char **argv)
 {
+	std::string command;
+	while (1)
+	{
+		std::cout << "Please enter ADD, SEARCH, or EXIT\n";
+		std::cin >> command;
+		if (valid_command(command))
+	}
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,30 +6,46 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 16:22:42 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/03/31 16:32:46 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/04/01 16:44:20 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+class PhoneBook
+{
+  private:
+	Contact contacts[8];
+	int count;
+	int oldestIndex;
+
+  public:
+	PhoneBook();
+
+	void addContact(const Contact &newContact);
+	void getAllContacts() const;
+	int getCount() const;
+};
+
+PhoneBook::PhoneBook()
+{
+	count = 0;
+	oldestIndex = 0;
+}
 class Contact
 {
   private:
 	std::string firstName;
 	std::string lastName;
+	std::string nickName;
 	std::string phoneNumber;
+	std::string darkestSecret;
 
   public:
 	// Constructor
-	Contact();
-
-	// Setter methods
-	void setFirstName(const std::string &name);
-	void setLastName(const std::string &name);
-	void setPhoneNumber(const std::string &number);
+	Contact::Contact(const std::string &firstName, const std::string &lastName,
+			const std::string &nickName, const std::string &darkestSecret);
 
 	// Getter methods
-	std::string getFirstName() const;
-	std::string getLastName() const;
-	std::string getPhoneNumber() const;
+	std::string getContact() const;
 };
